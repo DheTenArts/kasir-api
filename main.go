9d4a54acdb2c479e8c2d486c092851aa2ce4b592
DheTen <dhetenart01@gmail.com>
2026-01-31 18:33:26 +0800

    layered arch, implement env, database

--- main.go
+++ main.go
@@ -3,178 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
+	"kasir-api/database"
+	"kasir-api/handlers"
+	"kasir-api/repositories"
+	"kasir-api/services"
+	"log"
 	"net/http"
-	"strconv"
+	"os"
 	"strings"
 )
 
-type Categories struct {
-	ID    int    `json:"id"`
-	Nama  string `json:"nama"`
-	KAT string `json:"kat"`
-	Harga int    `json:"harga"`
-	Stok  int    `json:"stok"`
-}
-
-var kategori = []Categories{
-	{ID: 1, Nama: "Mie Goreng 100", KAT: "Makanan", Harga: 3000, Stok: 25},
-	{ID: 2, Nama: "Teh Sari Murni", KAT: "Minuman", Harga: 2500, Stok: 30},
-	{ID: 3, Nama: "Saos ABC", KAT: "Bumbu", Harga: 12000, Stok: 20},
-	{ID: 4, Nama: "Teh Gelas", KAT: "Minuman", Harga: 1000, Stok: 10},
-	{ID: 5, Nama: "Sarden DHE", KAT: "Makanan", Harga: 15000, Stok: 35},
-}
-
-// ! buat function untuk ambil kategori berdasarkan id
-func getCategoriesByID(w http.ResponseWriter, r *http.Request) {
-	// Parse ID dari URL path
-	// URL: /api/category/123 -> ID = 123
-	idStr := strings.TrimPrefix(r.URL.Path, "/api/category/")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "ID tidak angka", http.StatusBadRequest)
-		return
-	}
-
-	// Cari kategori dengan ID tersebut
-	for _, k := range kategori {
-		if k.ID == id {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(k)
-			return
-		}
-	}
-
-	// Kalau tidak found
-	http.Error(w, "Kategori belum ada", http.StatusNotFound)
-}
-
-// ! buat fucntion untuk update isi kategori berdasarkan id
-// ? localhost:8080/api/category/1
-/*
-	! PUT
-	! di postman pilih body lalu raw terus bagian json isi dibawah ini
-	! {
-	!	"nama" : "Frozen Food",
-	!	"harga": 25000,
-	!	"stok": 20
-	! }
-	! maka kategori id 1 akan terganti/terupdate isinya
-*/
-func updateCategory(w http.ResponseWriter, r *http.Request) {
-	// get id dari request
-	idStr := strings.TrimPrefix(r.URL.Path, "/api/category/")
-
-	// ganti int
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "ID tidak angka!", http.StatusBadRequest)
-		return
-	}
-
-	// get data dari request
-	var updateCategory Categories
-	err = json.NewDecoder(r.Body).Decode(&updateCategory)
-	if err != nil {
-		http.Error(w, "Request Gagal!", http.StatusBadRequest)
-		return
-	}
-
-	// loop kategori, cari id, ganti sesuai data dari request
-	for i := range kategori {
-		if kategori[i].ID == id {
-			updateCategory.ID = id
-			kategori[i] = updateCategory
-
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(updateCategory)
-			return
-		}
-	}
-
-	http.Error(w, "Kategori belum ada", http.StatusNotFound)
-}
-
-// ! buat fucntion untuk delete kategori berdasarkan id
-func deleteCategory(w http.ResponseWriter, r *http.Request) {
-	// get id
-	idStr := strings.TrimPrefix(r.URL.Path, "/api/category/")
-
-	// ganti id int
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "ID tidak angka!", http.StatusBadRequest)
-		return
-	}
-
-	// loop kategori cari ID, dapet index yang mau dihapus
-	for i, p := range kategori {
-		if p.ID == id {
-			// bikin slice baru dengan data sebelum dan sesudah index
-			kategori = append(kategori[:i], kategori[i+1:]...)
-
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(map[string]string{
-				"message": "sukses delete",
-			})
-			return
-		}
-	}
-
-	http.Error(w, "kategori belum ada", http.StatusNotFound)
+type Config struct {
+	Port   string `mapstructure:"PORT"`
+	DBConn string `mapstructure:"DB_CONN"`
 }
 
 func main() {
 
-	// ? GET localhost:8080/api/category/{id}
-	// ? PUT localhost:8080/api/category/{id}
-	// ? DELETE localhost:8080/api/category/{id}
-	http.HandleFunc("/api/category/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			getCategoriesByID(w, r)
-		} else if r.Method == "PUT" {
-			updateCategory(w, r)
-		} else if r.Method == "DELETE" {
-			deleteCategory(w, r)
-		}
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	})
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
 
-	// ! Method GET dan POST
-	/*
-		! POST
-		! di postman pilih body lalu raw terus bagian json isi dibawah ini
-		! {
-		!	"nama" : "Frozen Food",
-		!	"harga": 25000,
-		!	"stok": 20
-		! }
-		! maka akan nampil kategori baru dengan id 6
-	*/
-	// ! localhost:8080/api/category
-	http.HandleFunc("/api/category", func(w http.ResponseWriter, r *http.Request) {
-		// ? Ambil kategori dari var kategori
-		if r.Method == "GET" {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(kategori)
-		} else if r.Method == "POST" {
+	config := Config{
+		Port:   viper.GetString("PORT"),
+		DBConn: viper.GetString("DB_CONN"),
+	}
 
-			// ? buat variable baru
-			var CategoriesBaru Categories
-			err := json.NewDecoder(r.Body).Decode(&CategoriesBaru)
-			if err != nil {
-				http.Error(w, "Request gagal!", http.StatusBadRequest)
-				return
-			}
+	// Setup database
+	db, err := database.InitDB(config.DBConn)
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
+	defer db.Close()
 
-			// ? masukkin data ke dalam variable kategori
-			CategoriesBaru.ID = len(kategori) + 1
-			kategori = append(kategori, CategoriesBaru)
+	// ? deklarasi semua dari repositories, serivice, handlers
+	productRepo := repositories.NewProductRepository(db)
+	productService := services.NewProductService(productRepo)
+	productHandler := handlers.NewProductHandler(productService)
 
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated) // 201
-			json.NewEncoder(w).Encode(CategoriesBaru)
-		}
-	})
+	// ! Setup routes
+	http.HandleFunc("/api/produk", productHandler.HandleProducts)
+	http.HandleFunc("/api/produk/", productHandler.HandleProductByID)
 
 	// localhost:8080/health
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -184,9 +58,9 @@ func main() {
 			"message": "API Running",
 		})
 	})
-	fmt.Println("Server running di localhost:8080")
+	fmt.Println("Server running di localhost:" + config.Port)
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":"+config.Port, nil)
 	if err != nil {
 		fmt.Println("gagal running server")
 	}
